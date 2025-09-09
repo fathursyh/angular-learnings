f5ed8873a740ef9dd05b3a0d5e0bb3595ca1e583
Fathursyah Awandanu <fathur.syah04@gmail.com>
2025-09-09 09:32:41 +0700

    update: create user-input component

--- investment-section-4/src/app/user-input/user-input.ts
+++ investment-section-4/src/app/user-input/user-input.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.html',
   styleUrl: './user-input.css'
 })
 export class UserInput {
+  enteredInitialInvestment = '0';
+  annualInvestment = '0';
+  expectedReturn = '0';
+  duration = '0';
 
+  onSubmit() {
+    console.log('SUBMIT')
+  }
 }
